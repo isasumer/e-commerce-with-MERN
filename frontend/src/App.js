a4b2201858cc2ce9a3ab8402217823bc5add42af
Isa SUMER <sumerisa5@gmail.com>
2021-11-04 17:26:30 +0300

    header-footer created reactbootstap added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 const App = () => {
-  return <div>isa</div>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <h1>Welcome to ProShop</h1>
+        </main>
+      </Container>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
