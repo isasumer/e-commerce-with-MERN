8ac4a90e1d7f0c9399284b8ceb9a29f1f1340435
Isa SUMER <sumerisa5@gmail.com>
2021-11-04 18:54:18 +0300

    HomeScreen component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <Container>
         <main className="py-3">
-          <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </main>
       </Container>
       <Footer />
