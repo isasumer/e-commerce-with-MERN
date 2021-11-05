256c8a2b4ffb785e225c0df02bd7144d319908c4
Isa SUMER <sumerisa5@gmail.com>
2021-11-05 14:19:23 +0300

    header redesigned

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, BrowseRouter as Router } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Header = () => {
@@ -7,22 +7,22 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Link to="/">
-            <Navbar.Brand>ProShop</Navbar.Brand>
-          </Link>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Link to="/cart">
-                <Nav.Link>
-                  <i className="fas fa-shopping-cart"></i> Cart
-                </Nav.Link>
-              </Link>
-              <Link to="/login">
-                <Nav.Link>
-                  <i className="fas fa-user pe-2"></i>Sign In
-                </Nav.Link>
-              </Link>
+              <Router>
+                <Link to="/cart">
+                  <div>
+                    <i className="fas fa-shopping-cart"></i> Cart
+                  </div>
+                </Link>
+                <Link to="/login">
+                  <div>
+                    <i className="fas fa-user pe-2"></i>Sign In
+                  </div>
+                </Link>
+              </Router>
             </Nav>
           </Navbar.Collapse>
         </Container>
