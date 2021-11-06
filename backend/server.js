a75e335e0fb70f928ffaf810589c0686863b2476
Isa SUMER <sumerisa5@gmail.com>
2021-11-06 09:50:59 +0300

    "dotenv": "^10.0.0", package installed and .env createad. Port address and other token infos written in .env and in server js made some differences

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const products = require("./data/products");
 
 const app = express();
+
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("API is running.....");
@@ -14,4 +17,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
